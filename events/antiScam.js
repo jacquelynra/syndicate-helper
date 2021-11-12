554f4fd266f0045cb67d898ee0a2cfe244e670c1
Mankeke <74993776+mankewhe@users.noreply.github.com>
2021-11-12 20:24:37 -0300

    Create antiScam.js

---
+++ events/antiScam.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js');
+const links = require('./configs/links');
+
+module.exports = {
+  name: 'messageCreate',
+  async execute(message) {
+    
+    // Delete message function
+    function deleteMessage() {
+    message.delete()
+    if(message.member.permissions.has('ADMINISTRADOR')) return;
+    message.guild.members.ban(message.member.id, { reason:"Scam Link" })
+  }
+    
+    // Catch the Link
+    if (links.some(word => message.content.toLowerCase().includes(word))) { // See if the message contains a link 
+    deleteMessage() //// Delete message link
+  }
+    
+  }
+}
